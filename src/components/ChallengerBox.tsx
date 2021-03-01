a370f482b9e4b1963b88c68a6e5e29463bc26145
Felipe Santos <fezto.17@gmail.com>
2021-02-28 22:53:37 -0300

    Business rules finalized

--- src/components/ChallengerBox.tsx
+++ src/components/ChallengerBox.tsx
@@ -1,10 +1,27 @@
 import { useContext } from 'react';
-import { ChallengesContext } from '../contexts/ChallengedsContext';
+import { ChallengesContext } from '../contexts/ChallengesContext';
+import { CountdownContext } from '../contexts/CountdownContext';
 
 import styles from '../styles/components/ChallengerBox.module.css';
 
 export function ChallengerBox() {
-  const { activeChallenge, resetChallenge } = useContext(ChallengesContext);
+  const {
+    activeChallenge,
+    resetChallenge,
+    completeChallenge
+  } = useContext(ChallengesContext);
+
+  const { resetCoutdown } = useContext(CountdownContext);
+
+  function handleChallengeSucceeded() {
+    completeChallenge();
+    resetCoutdown();
+  }
+
+  function handleChallengeFailed() {
+    resetChallenge();
+    resetCoutdown();
+  }
 
   return (
     <div className={styles.challengeBoxContainer}>
@@ -22,13 +39,14 @@ export function ChallengerBox() {
             <button
               type="button"
               className={styles.challengeFailedButton}
-              onClick={resetChallenge}
+              onClick={handleChallengeFailed}
             >
               Falhei
             </button>
             <button
               type="button"
               className={styles.challengeSucceededButton}
+              onClick={handleChallengeSucceeded}
             >
               Completei
             </button>
---
+++ src/contexts/CountdownContext.tsx
@@ -0,0 +1,66 @@
+import { createContext, ReactNode, useContext, useEffect, useState } from "react";
+import { ChallengesContext } from "./ChallengesContext";
+
+interface CoutdownContextData {
+  minutes: number;
+  seconds: number;
+  hasFinished: boolean;
+  isActive: boolean;
+  startCoutdown: () => void;
+  resetCoutdown: () => void;
+}
+
+interface CountdownProviderProps {
+  children: ReactNode;
+}
+
+let countdownTimeout: NodeJS.Timeout;
+
+export const CountdownContext = createContext({} as CoutdownContextData)
+
+export function CountdownProvider({ children }: CountdownProviderProps) {
+  const { startNewChallenge } = useContext(ChallengesContext);
+
+  const [time, setTime] = useState(0.1 * 60);
+  const [isActive, setIsActive] = useState(false);
+  const [hasFinished, setHasFinished] = useState(false);
+
+  const minutes = Math.floor(time / 60);
+  const seconds = time % 60;
+
+  function startCoutdown() {
+    setIsActive(true);
+  }
+
+  function resetCoutdown() {
+    clearTimeout(countdownTimeout);
+    setIsActive(false);
+    setHasFinished(false);
+    setTime(0.1 * 60);
+  }
+
+  useEffect(() => {
+    if (isActive && time > 0) {
+      countdownTimeout = setTimeout(() => {
+        setTime(time - 1);
+      }, 1000)
+    } else if (isActive && time === 0) {
+      setHasFinished(true);
+      setIsActive(false);
+      startNewChallenge();
+    }
+  }, [isActive, time])
+
+  return (
+    <CountdownContext.Provider value={{
+      minutes,
+      seconds,
+      hasFinished,
+      isActive,
+      startCoutdown,
+      resetCoutdown,
+    }}>
+      {children}
+    </CountdownContext.Provider>
+  )
+}
